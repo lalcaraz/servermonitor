21e6db61bf0f3e41b7cb667cf2ccf4e7b6c8280d
Luis Alcaraz <me@lalcaraz.com>
2020-08-19 12:16:00 -0400

    Adding suggested changes to crontab and a server monitoring tool.

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/aristocratos/bashtop
 git clone https://github.com/Matty9191/ssl-cert-check.git
+git clone https://github.com/Freemius/server-monitoring
 apt install cowsay s3cmd
